adc5d998203352d22f0a992c23d5063e1f8c991c
pankaj-mehta <pankajmehta40@gmail.com>
2020-07-13 23:52:13 +0530

    first upload

---
+++ index.js
@@ -0,0 +1,74 @@
+/* Detecting mouse click */
+
+var allbuttons = document.querySelectorAll(".drum");
+var i;
+for (i = 0; i < allbuttons.length; i++) {
+  allbuttons[i].addEventListener("click", function (){
+    var buttoninnerhtml = this.innerHTML;
+    makesound(buttoninnerhtml);
+    buttonanimation(buttoninnerhtml);
+  });
+}
+
+/* Detecting keyboard */
+
+document.addEventListener("keypress",function(event){
+  makesound(event.key);
+  buttonanimation(event.key);
+})
+
+
+
+/* Sound producing function */
+
+function makesound(key){
+  switch (key) {
+    case "w":
+    var tom1 = new Audio('sounds/tom-1.mp3');
+    tom1.play();
+    break;
+
+    case "a":
+    var tom2 = new Audio('sounds/tom-2.mp3');
+    tom2.play();
+    break;
+
+    case "s":
+    var tom3 = new Audio('sounds/tom-3.mp3');
+    tom3.play();
+    break;
+
+    case "d":
+    var tom4 = new Audio('sounds/tom-4.mp3');
+    tom4.play();
+    break;
+
+    case "j":
+    var crash = new Audio('sounds/crash.mp3');
+    crash.play();
+    break;
+
+    case "k":
+    var kick = new Audio('sounds/kick-bass.mp3');
+    kick.play();
+    break;
+
+    case "l":
+    var snare = new Audio('sounds/snare.mp3');
+    snare.play();
+    break;
+
+    default: console.log(buttoninnerhtml);
+  }
+}
+
+/* Animation to buttons when pressed*/
+
+function buttonanimation(currentkey){
+  var activebutton = document.querySelector("." + currentkey);
+  activebutton.classList.add("pressed");
+
+  setTimeout(function(){
+    activebutton.classList.remove("pressed");
+  }, 100);
+}
